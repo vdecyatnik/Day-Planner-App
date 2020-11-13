1b02af183d542bd6e9d1a4abca9c3d75002736d9
Val <vdecyatnik@gmail.com>
2020-11-13 10:11:51 -0800

    HTML and pseudo

---
+++ Develop/javascript.js
@@ -0,0 +1,27 @@
+//  Get the current hour of the day using moment.js
+
+//  Loop over the hours of the day from i=9 to i <-=17
+
+//  var i =9
+//  Element Id = "hour-" +i
+
+//  Currently checking 9AM< current hour
+//equal to or greater than current hour
+
+//  9AM is in the past 
+//  Select matching element $(ElementID).addClass("is-past"); OR
+// $(ElementId).css("background");
+
+// .is-past, is-present, is-future
+
+//  $( Element ID)
+
+// add a click event that can listen to the save buttons
+
+// add  a click event on a parent element that can listen to my save button clicks
+
+//How to get from correct button to the hour of the day so save to the correct location
+
+//use object or array to save data and use json stringify to get it out of local storage
+
+//
