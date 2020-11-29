ddf5946d3e140fed60d3934f96572b27212832f0
Val <vdecyatnik@gmail.com>
2020-11-28 16:02:21 -0800

    made responsive

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -14,6 +14,10 @@ console.log(today);
 var currentHourOne = parseInt(moment().format("H"));
 console.log(currentHourOne);
 
+
+
+
+
 //Loop over the hours of 9a to 5pm
 
 //var i=9;
@@ -23,68 +27,78 @@ console.log(currentHourOne);
 //$(elementId)
 
 
-for (var i=9;i<18;i++){
+//function showSavedPlanArray(){
 
 
+//var showPlans= JSON.parse(localStorage.getItem(savedPlanKey));
+
+//console.log(showPlans);
+
+
+
+
+//}
+
+for (var i = 9; i < 18; i++) {
+
+    //comparing moment JS currentHourOne to current hour on the planner.
+    //Used id= "hour-9" +i; to loop through 9am-5pm
     var elementId = $("#hour-" + i);
     var currentHourOne = parseInt(moment().format("H"));
 
-    if (i<currentHourOne){
+    if (i < currentHourOne) {
         $(elementId).addClass("past");
 
-    } if (i>currentHourOne ){
+    } if (i > currentHourOne) {
         $(elementId).addClass("future");
     } if (i === currentHourOne) {
         $(elementId).addClass("present");
     }
     console.log(elementId);
 
-
-
-
-
-
-
-
+     if (localStorage.getItem("saved-hour-" + i)){
+        $("#data-text-" + i).val(localStorage.getItem("saved-hour-" + i))
+     }
+     
 
 
 }
-
-
-
 //get from local storage and navigate to text area and assign the value
 // add  a click event ON A PARENT ELEMENT that can listen to my save button clicks
 // Added eventListener to parents Element 
 // Save text-input to local storage
-$(".saveBtn").on("click" ,  function (e) {
-    
-console.log("clicked element");
+
+//added a click event on the save buttons to and matched them to the current hour using data-hour="9"
+$(".saveBtn").on("click", function (e) {
+
+    console.log("clicked element");
 
 
-var clickedSaveButton = $(event.target).data("hour")
-console.log(clickedSaveButton);
-var value =$(event.target).prev().val();
+    var clickedSaveButton = $(event.target).data("hour")
+    console.log(clickedSaveButton);
+    var value = $(event.target).prev().val();
 
-var planAdded = $("#text").val();
-console.log(planAdded);
+    var planAdded = $("#textInput").val();
+    console.log(planAdded);
 
-localStorage.setItem( clickedSaveButton, value);
-//localStorage.setItem( )
+    localStorage.setItem("saved-hour-" + clickedSaveButton, value);
+    console.log(localStorage);
+
+})
 
 
-});
+function clearLocalStorage(){
+
    
-//function saveInfo(){
+    localStorage.clear();
+ 
+}
 
-//How to get from correct button to the hour of the day so save to the correct location
+
+    
+
 
 //use object or array to save data and use json stringify to get it out of local storage
-
-//WHEN the user clicks a save button 
-
-
 //OR var the ClickedHour = $(event.target).parent().attr("id")
-
 //var value =$(event.target).prev().val();
-
 //localStorage.setItem("planner-hour-"+ theClickedHour , value)
